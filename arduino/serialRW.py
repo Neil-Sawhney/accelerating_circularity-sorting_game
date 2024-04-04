20a946eb69b277e83a946289907d35b5015401fd
Neil Sawhney <Neil@neil-sawhney.com>
2024-04-04 13:51:15 -0400

    comunication protocol implemented in ui

--- arduino/serialRW.py
+++ arduino/serialRW.py
@@ -1,4 +1,5 @@
 import logging
+from enum import Enum
 
 import serial
 
@@ -9,11 +10,53 @@ import GUI.errorDisplay as eDisp
 ser = serial.Serial()
 
 
+class Cmd(Enum):
+    STATUS = 0
+    TARGET = 1
+    CARD_INFO = 2
+    TARGET_HIT = 3
+    LOGGING = 4
+
+
+class Status(Enum):
+    READY = 0
+    RESET = 1
+    NONE = 2
+
+
+class Button(Enum):
+    BUTTON_0 = 0
+    BUTTON_1 = 1
+    BUTTON_2 = 2
+    BUTTON_3 = 3
+    BUTTON_4 = 4
+    BUTTON_5 = 5
+    NONE = 6
+
+
 def RData():
-    # data comes in as a string of the form "cmd,message"
+    # get a line from the arduino
     serialData = ser.readline().decode("utf-8")
     logging.debug("RX:" + serialData)
-    cmd, message = serialData.split(",")
+    rx_cmd, rx_message = serialData.split(",")
+    cmd = Cmd(int(rx_cmd))
+
+    # while the data recievied is a logging command, print the data to the log file and get the next line
+    while cmd == Cmd.LOGGING:
+        logging.debug("Arduino:" + rx_message)
+
+        # get a line from the arduino
+        serialData = ser.readline().decode("utf-8")
+        logging.debug("RX:" + serialData)
+        rx_cmd, rx_message = serialData.split(",")
+        cmd = Cmd(int(rx_cmd))
+
+    if cmd == Cmd.STATUS:
+        message = Status(int(rx_message))
+    elif cmd == Cmd.TARGET_HIT:
+        message = Button(int(rx_message))
+    else:
+        message = rx_message
 
     return cmd, message
 
