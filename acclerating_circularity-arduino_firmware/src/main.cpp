9c48e35644b2e1307b99af2ee181b82478dc04a6
Neil Sawhney <Neil@neil-sawhney.com>
2024-04-15 19:31:31 -0400

    reminders for implemenetation details on the rduino side of things

--- acclerating_circularity-arduino_firmware/src/main.cpp
+++ acclerating_circularity-arduino_firmware/src/main.cpp
@@ -9,8 +9,10 @@ void setup()
   gpio_init();
 }
 
+//TODO: add functionality rto turn the tech on and off which basically means deciding wheter we should illuminate the target button or not
 void loop()
 {
+  //TODO: only send READY at the beginning of the game, right after the flashing start button is pressed. implement all that
   writeSerial(Cmd::STATUS, Status::READY);
   waitForStatus();
 
